3e4540eaa399d3c6ff386adc9b0e55159adc94fa
jrucl2d <ysk789465@gmail.com>
2020-09-13 21:17:03 +0900

    server running test, build test

--- src/main/java/hello/hellospring/Controller/HelloController.java
+++ src/main/java/hello/hellospring/Controller/HelloController.java
@@ -7,9 +7,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HelloController {
 
-    @GetMapping("hello")
+    @GetMapping("hello") // get 방식으로 /hello 주소를 받음
     public String hello(Model model){
+        // 템플릿 엔진으로 변수 넘겨주기
         model.addAttribute("data", "hello!!");
-        return "hello";
+        return "hello"; // hello라는 이름의 템플릿 엔진 찾아서 렌더링
     }
 }
