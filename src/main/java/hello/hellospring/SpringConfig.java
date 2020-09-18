30eb2e9b8deb36c5a0c8292d1f9d6f99e7dd5005
jrucl2d <ysk789465@gmail.com>
2020-09-18 14:06:40 +0900

    Using h2 database done

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,18 +1,28 @@
 package hello.hellospring;
 
+import hello.hellospring.repository.JdbcMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+    
+    private DataSource datasource; // 데이터 소스를 가져옴
+
+    public SpringConfig(DataSource datasource) {
+        this.datasource = datasource;
+    }
 
     // 직접 스프링 빈을 설정
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+//        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(datasource); // 여기를 Jdbc로만 연결해주면 됨
     }
     @Bean
     public MemberService memberService(){
