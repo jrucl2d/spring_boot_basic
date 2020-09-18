858a2cf286365f61b6dc6f4350884b4bb0d0325f
jrucl2d <ysk789465@gmail.com>
2020-09-18 15:41:04 +0900

    Studied spring data JPA

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,14 +1,11 @@
 package hello.hellospring;
 
-import hello.hellospring.repository.JdbcTemplateMemberRepository;
 import hello.hellospring.repository.JpaMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import javax.persistence.EntityManager;
 
 @Configuration
 public class SpringConfig {
@@ -19,24 +16,35 @@ public class SpringConfig {
 //        this.datasource = datasource;
 //    }
 
-    private EntityManager em;
+//    private EntityManager em;
+//
+//    @Autowired
+//    public SpringConfig(EntityManager em) {
+//        this.em = em;
+//    }
+
+    // interface만 만들어놔도 알아서 구현체를 만들기 때문에 바로 DI해주면 된다.
+    private final MemberRepository memberRepository;
 
     @Autowired
-    public SpringConfig(EntityManager em) {
-        this.em = em;
-    }
-
-
-    // 직접 스프링 빈을 설정
-    @Bean
-    public MemberRepository memberRepository() {
-//        return new MemoryMemberRepository();
-//        return new JdbcMemberRepository(datasource); // 여기를 Jdbc로만 연결해주면 됨
-//        return new JdbcTemplateMemberRepository(datasource);
-        return new JpaMemberRepository(em);
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
     @Bean
     public MemberService memberService(){
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
+
+    // 직접 스프링 빈을 설정
+//    @Bean
+//    public MemberRepository memberRepository() {
+////        return new MemoryMemberRepository();
+////        return new JdbcMemberRepository(datasource); // 여기를 Jdbc로만 연결해주면 됨
+////        return new JdbcTemplateMemberRepository(datasource); // JdbcTemplate 쓰면
+//        return new JpaMemberRepository(em); // JPA 사용하면
+//    }
+//    @Bean
+//    public MemberService memberService(){
+//        return new MemberService(memberRepository()); // 스프링 데이터 JPA에서는 필요 없다.
+//    }
 }
